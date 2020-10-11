91e196096dae9b41299be8b0a0462f7c8f76d441
Natalia Furmanczyk <bells.berryd@gmail.com>
2020-10-10 18:12:18 -0700

    Add project files.

---
+++ Conex 1/Conex 1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Conex_1
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrementCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = count.ToString();
+        }
+    }
+}
